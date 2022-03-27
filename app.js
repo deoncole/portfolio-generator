68b841eb839c44457c833bb20911c38da0bc30ef
Cole <deoncole77@gmail.com>
2022-03-27 09:54:45 -0400

    added html page and uses the new promises

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // create a constant that takes in the inquirer npm package
 const inquirer = require('inquirer');
 // create a constant that access the fileSystem (fs)
-const fs = require('fs');
+// const fs = require('fs');
 // get the file that hold the generate page function
 const generatePage = require('./src/page-template.js');
+// import the file to write to the documents
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 // the first argument is the file name to be created(output file), the second is the data that's being written, and the third is the callback function to handle errors
 // fs.writeFile('index.html', generatePage(name, github), err => {
@@ -145,11 +147,29 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
+  // .then(portfolioData => {
+  //   const pageHTML = generatePage(portfolioData);
+
+  //   fs.writeFile('./dist/index.html', pageHTML, err => {
+  //     if (err) {
+  //       console.log(err);
+  //       return;
+  //     }
+  //     console.log('Page created! Check out index.html in this directory to see it!');
+  //   });
+  // });
